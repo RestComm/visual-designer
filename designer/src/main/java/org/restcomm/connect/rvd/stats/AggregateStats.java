02af6852f74af3b1866f23e3fb0fb0cb136fd3dc
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2017-08-03 12:44:09 +0300

    Secured /stats REST service
    Refers #184

--- designer/src/main/java/org/restcomm/connect/rvd/stats/AggregateStats.java
+++ designer/src/main/java/org/restcomm/connect/rvd/stats/AggregateStats.java
@@ -90,16 +90,4 @@ public class AggregateStats {
      */
     public AtomicInteger esCallsSuccess;
 
-    /**
-     * Number of ES calls for which an HTTP request was made and a response is received or an error is
-     * thrown (i.e. they are no more blocked).
-     *
-     * This metric is used in conjuction with esCallsTotalDurationMilis to calculate average execution time
-     */
-    //public AtomicInteger esCallsRequested;
-
-    /**
-     *
-     */
-    //public AtomicLong esCallsTotalDurationMilis;
 }
