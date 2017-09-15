ca40ffff47fb94b2c334b319f5f93298d50eb986
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2017-09-15 12:20:33 +0300

    WIP

---
+++ designer/src/test/java/org/restcomm/connect/rvd/model/steps/say/SayStepTest.java
@@ -0,0 +1,26 @@
+package org.restcomm.connect.rvd.model.steps.say;
+
+import org.junit.Test;
+import org.restcomm.connect.rvd.ApplicationContext;
+import org.restcomm.connect.rvd.interpreter.Interpreter;
+
+/**
+ * @author otsakir@gmail.com - Orestis Tsakiridis
+ */
+public class SayStepTest {
+
+    Interpreter interpreter;
+
+    public SayStepTest() {
+        ApplicationContext appContext = new ApplicationContext();
+
+        //interpreter = new Interpreter();
+    }
+
+    @Test
+    public void defaultRendering() {
+        SayStep step = new SayStep();
+
+
+    }
+}
