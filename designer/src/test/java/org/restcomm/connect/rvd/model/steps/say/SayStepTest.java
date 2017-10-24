6a36687f8f98c48b7b232d7cd6b9fef801fc4c27
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2017-10-24 09:54:17 +0300

    SayStep test case for variable support in phrase
    Refers #244

--- designer/src/test/java/org/restcomm/connect/rvd/model/steps/say/SayStepTest.java
+++ designer/src/test/java/org/restcomm/connect/rvd/model/steps/say/SayStepTest.java
@@ -1,14 +1,29 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
 package org.restcomm.connect.rvd.model.steps.say;
 
 import junit.framework.Assert;
 import org.junit.Test;
-import org.restcomm.connect.rvd.ApplicationContext;
 import org.restcomm.connect.rvd.RvdConfiguration;
 import org.restcomm.connect.rvd.interpreter.Interpreter;
-import org.restcomm.connect.rvd.logging.MockedCustomLogger;
-import org.restcomm.connect.rvd.logging.ProjectLogger;
-import org.restcomm.connect.rvd.logging.system.LoggingContext;
-import org.restcomm.connect.rvd.model.ProjectSettings;
 import org.restcomm.connect.rvd.model.steps.StepTestBase;
 
 /**
@@ -16,11 +31,9 @@ import org.restcomm.connect.rvd.model.steps.StepTestBase;
  */
 public class SayStepTest extends StepTestBase {
 
-    Interpreter interpreter;
-
-    public SayStepTest() {
-    }
-
+    /**
+     * Check if all properties of a Say step get rendered
+     */
     @Test
     public void defaultRendering() {
         Interpreter interpreter = buildInterpreter(appendMultivaluedMap(null,null,null));
@@ -31,9 +44,13 @@ public class SayStepTest extends StepTestBase {
         Assert.assertEquals("man", rcmlStep.getVoice());
     }
 
+    /**
+     * Checks if a phrase of a Say step supports variables.
+     */
     @Test
-    public void phraseWithVariables() {
-        Interpreter interpreter = buildInterpreter(appendMultivaluedMap(null, RvdConfiguration.MODULE_PREFIX + "name","alice"));
+    public void phraseWithModuleVariable() {
+        Interpreter interpreter = buildInterpreter(appendMultivaluedMap(null,null,null));
+        interpreter.getVariables().put(RvdConfiguration.MODULE_PREFIX + "name", "alice");
         SayStep step = new SayStep("hello $name", null, null, null);
         RcmlSayStep rcmlStep = (RcmlSayStep)step.render(interpreter, null);
         Assert.assertEquals("hello alice", rcmlStep.getPhrase());
