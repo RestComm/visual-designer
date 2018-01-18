87e4ada7e316246fc6f2eac68090507fca33a3f3
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2018-01-18 14:45:27 +0200

    Eliminated caching issues by using cacheBust
    In detail:
    
    - Added grunt and grunt cacheBust plugin
    - Added frontend and war maven plugins
    - Moved .js libraries under a separate folder so that they are not picked up by cacheBust
    
    Refers #88

---
+++ designer/Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+      grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: ['target/dist'],
+        copy: {
+          main: {
+            expand: true,
+            cwd: 'src/main/webapp',
+            src: '**',
+            dest: 'target/dist',
+          },
+        },
+        cacheBust: {
+            taskName: {
+                options: {
+                    assets: ['js/app/**/*', 'css/*'],
+                    baseDir: 'target/dist/',
+                    outputDir: 'assets/',
+                    clearOutputDir: true,
+                    deleteOriginals: true,
+                    queryString: false
+                },
+                files: [{
+                  src: ['target/dist/index.html']
+                }]
+            }
+        },
+        ngtemplates:  {
+          Rvd: {
+            cwd:      'target/dist',
+            src:      'templates/**/*.html',
+            dest:     'target/dist/app.js',
+            options:    {
+              append: true
+            }
+          }
+        },
+      });
+
+      grunt.loadNpmTasks('grunt-angular-templates');
+      grunt.loadNpmTasks('grunt-contrib-clean');
+      grunt.loadNpmTasks('grunt-contrib-copy');
+      grunt.loadNpmTasks('grunt-cache-bust');
+
+      grunt.registerTask('default', ['clean', 'copy', 'ngtemplates', 'cacheBust']);
+
+};
+
