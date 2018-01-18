ed3c9ebec8a6323e48e34909a526f5d0e091e39c
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2018-01-18 10:45:28 +0200

    hide 'Parameters' link if parameters are available
    Refers #281

--- designer/src/main/webapp/js/app/controllers/controllers.js
+++ designer/src/main/webapp/js/app/controllers/controllers.js
@@ -84,7 +84,7 @@ App.controller('AppCtrl', function ($rootScope, $location, $scope, Idle, keepAli
     });
 });
 
-angular.module('Rvd').controller('designerMainmenuCtrl', function ($scope, $stateParams, project,$rootScope, designerService, projectSettingsService, parametersService) {
+angular.module('Rvd').controller('designerMainmenuCtrl', function ($scope, $stateParams, project,$rootScope, designerService, projectSettingsService, parametersService, parametersResource) {
 	$scope.projectName = $stateParams.projectName;
 	$scope.applicationSid = $stateParams.applicationSid;
     $scope.project = project;
@@ -96,6 +96,12 @@ angular.module('Rvd').controller('designerMainmenuCtrl', function ($scope, $stat
 	$scope.showParameters = function () {
 	  parametersService.showModal($scope.applicationSid);
 	}
+
+	parametersResource.get({applicationSid: $stateParams.applicationSid},function (data) {
+    if (data.parameters.length > 0) {
+      $scope.showParametersButton = true;
+    }
+  });
 
 	$scope.toggleShowGraph = function () {
 	    $scope.showGraph = !$scope.showGraph;
