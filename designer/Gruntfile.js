558b0baf4ef0042d2fa31c1519706cc2ae11efdd
Orestis Tsakiridis <otsakir@gmail.com>
2018-03-05 09:56:10 +0200

    Keep original .css files to handle not to break dynamically generated links for theming
    Refers RestComm/visual-designer#308

--- designer/Gruntfile.js
+++ designer/Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
                     baseDir: 'target/dist/',
                     outputDir: 'assets/',
                     clearOutputDir: true,
-                    deleteOriginals: true,
+                    deleteOriginals: false,
                     queryString: false
                 },
                 files: [{
